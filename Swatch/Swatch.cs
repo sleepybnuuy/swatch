590b64ea3437fdeb90f8e7ccea0525b144ed7cf4
U-DESKTOP-6N0K43M\bluec <sleepybnuuy@gmail.com>
2026-01-04 12:46:45 -0600

    swatch init

---
+++ Swatch/Swatch.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui.Dtr;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+
+using Swatch.Windows;
+
+namespace Swatch;
+
+public sealed class Swatch : IDalamudPlugin {
+	[PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+	[PluginService] internal static IFramework Framework { get; private set; } = null!;
+	[PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+	[PluginService] internal static IPluginLog Log { get; private set; } = null!;
+	[PluginService] internal static IDtrBar DtrBar { get; private set; } = null!;
+
+	private readonly WindowSystem _windowSystem = new("Swatch");
+	private readonly IDtrBarEntry _dtrEntry;
+
+	private int _beats;
+	private DateTime _nextUpdateAfter;
+
+	private const string CommandName = "/swatch";
+	private const string DtrTitle = "Swatch";
+
+	public Configuration Configuration { get; init; }
+
+	private ConfigWindow ConfigWindow { get; init; }
+
+	public Swatch() {
+		this.Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+
+		this.ConfigWindow = new ConfigWindow(this);
+		this._windowSystem.AddWindow(this.ConfigWindow);
+		PluginInterface.UiBuilder.Draw += this._windowSystem.Draw;
+		PluginInterface.UiBuilder.OpenConfigUi += this.ToggleConfigUi;
+
+		CommandManager.AddHandler(CommandName, new CommandInfo(this.OnCommand) {
+			HelpMessage = "open swatch-dtr config"
+		});
+
+		this._dtrEntry = DtrBar.Get(DtrTitle);
+		this._dtrEntry.Tooltip = ".beat time";
+		this._dtrEntry.OnClick = args => this.ConfigWindow.Toggle();
+
+		this._beats = GetBeats();
+		this.SetNextUpdate();
+		this.SetLabel();
+
+		Framework.Update += this.SwatchUpdate;
+
+		Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+	}
+
+	private void OnCommand(string command, string args) => this.ToggleConfigUi();
+
+	private void ToggleConfigUi() => this.ConfigWindow.Toggle();
+
+	private void SwatchUpdate(IFramework _) {
+		if (DateTime.UtcNow < this._nextUpdateAfter) return; // skip updating if we're too far from the beat change window
+
+		var newBeat = GetBeats();
+		if (newBeat <= this._beats) return; // if we get the same beat, don't fire an update
+
+		this._beats = newBeat;
+		this.SetNextUpdate();
+		this.SetLabel();
+	}
+
+	private static int GetBeats() {
+		var ms = GetUtcMs();
+		return MsToBeats(ms);
+	}
+
+	private static int GetUtcMs() {
+		var time = DateTime.UtcNow;
+		time += TimeSpan.FromHours(1);
+		return ((time.Hour * 60 + time.Minute) * 60 + time.Second) * 1000 + time.Millisecond;
+	}
+
+	private static int MsToBeats(int ms) => (int)Math.Floor((double)Math.Abs(ms / 86400));
+
+	public void SetLabel() {
+		var label = "";
+		if (this.Configuration.ShowInternetLabel)
+			label += "internet time ";
+		label += "@ " + this._beats.ToString(CultureInfo.InvariantCulture);
+		this._dtrEntry.Text = label;
+	}
+
+	private void SetNextUpdate() {
+		var targetBeat = this._beats + 1;
+		var msDiff = (targetBeat * 86400) - GetUtcMs();
+		this._nextUpdateAfter = DateTime.UtcNow.AddMilliseconds(msDiff - 100); // 0.1ms wiggle room
+	}
+
+	public void Dispose() {
+		// Unregister all actions to not leak anything during disposal of plugin
+		PluginInterface.UiBuilder.Draw -= this._windowSystem.Draw;
+		PluginInterface.UiBuilder.OpenConfigUi -= this.ToggleConfigUi;
+
+		this._windowSystem.RemoveAllWindows();
+		this.ConfigWindow.Dispose();
+
+		this._dtrEntry.Remove();
+
+		CommandManager.RemoveHandler(CommandName);
+	}
+}
---
+++ Swatch/Windows/ConfigWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Windowing;
+
+namespace Swatch.Windows;
+
+public class ConfigWindow : Window, IDisposable {
+	private readonly Configuration _config;
+	private readonly Swatch _plugin;
+
+	public ConfigWindow(Swatch plugin) : base("Swatch Config") {
+		this._config = plugin.Configuration;
+		this._plugin = plugin;
+	}
+
+	public void Dispose() { }
+
+	public override void Draw() {
+		var updated = false;
+		ImGui.TextWrapped("Swatch is a DTR (Server Info Bar) plugin to show the current time in Swatch's .beats format, inspired by Phantasy Star Online.");
+		ImGui.TextWrapped("Each .beat is 86.4 seconds long, with 1000 .beats in a day - every region globally has the same .beat time, so you can coordinate across the web without timezone considerations.");
+		ImGui.TextWrapped("You can edit the ordering of your DTR-bar in Dalamud's Server Info Bar settings.");
+
+		ImGui.Spacing();
+		ImGui.Separator();
+		ImGui.Spacing();
+
+		var showInternet = this._config.ShowInternetLabel;
+		if (ImGui.Checkbox("Show 'internet time' label", ref showInternet)) {
+			this._config.ShowInternetLabel = showInternet;
+			this._plugin.SetLabel();
+			updated = true;
+		}
+
+		if (updated) this._config.Save();
+	}
+}
